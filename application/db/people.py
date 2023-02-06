be280f0d6408b5b1c8ec01fa7162a92613a6b2f4
TimofeevAndrei <i.pika4u@gmail.com>
2023-02-06 12:49:39 +0700

    all ok

--- application/db/people.py
+++ application/db/people.py
@@ -1,2 +1,6 @@
 def get_employees(x):
     print(x)
+
+
+if __name__ == '__main__':
+    get_employees()
--- application/salary.py
+++ application/salary.py
@@ -1,3 +1,5 @@
 def calculate_salary(x):
     print(x)
 
+if __name__ == '__main__':
+    calculate_salary()
